a6907fc58423a04ed0bfc2ede3ce653bc84a8132
SteadfastSon <AlexanderSunstone@outlook.com>
2017-04-26 00:34:41 -0600

    just added comment of OK IT WILL at OnSleep()

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -23,7 +23,7 @@ namespace App1
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
+            // Handle when your app sleeps: OK it will
         }
 
         protected override void OnResume()
